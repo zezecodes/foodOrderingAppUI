f0747b00b0a1cc3efe24f04facd52abbc0a1878b
Aaron Djangmah <96618832+zezecodes@users.noreply.github.com>
2023-12-13 10:07:18 +0000

    updates

--- lib/Screens/Homescreen/Homepage/banner_picture.dart
+++ lib/Screens/Homescreen/Homepage/banner_picture.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class BannerPicture extends StatefulWidget {
   final String? picture;
-  const BannerPicture({super.key,required this.picture});
+  const BannerPicture({super.key, required this.picture});
 
   @override
   State<BannerPicture> createState() => _BannerPictureState();
@@ -12,12 +12,14 @@ class _BannerPictureState extends State<BannerPicture> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.01),
+      padding: EdgeInsets.symmetric(
+          horizontal: MediaQuery.of(context).size.width * 0.03),
       child: Container(
-        width: 335,
+        width: MediaQuery.of(context).size.width * 5,
         height: 185,
         decoration: BoxDecoration(
-            image: DecorationImage(image: AssetImage(widget.picture!)),
+            image: DecorationImage(
+                image: AssetImage(widget.picture!), fit: BoxFit.cover),
             borderRadius: BorderRadius.circular(20)),
       ),
     );
